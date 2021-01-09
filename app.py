e8ba14815a162e1a2d46c4cc225633c3f19e7890
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2021-01-09 19:20:09 +0600

    csv upload added

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
 from flask import Flask, render_template, request, url_for, redirect
 import pickle
+import io
+import csv
 
 app = Flask(__name__)
 
+model = pickle.load(open('evc.pickle', 'rb'))
+
 names = ["Age", "Blood Pressure", "Specific Gravity", "Albumin", "Sugar", "Red Blood Cells", "Pus Cell", "Pus Cell clumps", "Bacteria", "Blood Glucose Random", "Blood Urea", "Serum Creatinine", "Sodium", "Potassium", "Hemoglobin", "Packed Cell Volume", "White Blood Cell Count", "Red Blood Cell Count", "Hypertension", "Diabetes Mellitus", "Coronary Artery Disease", "Appetite", "Pedal Edema", "Anemia"]
+
+min = [0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2000, 0, 0, 0, 0, 0, 0, 0]
+max = [120, 360, 1.1, 5.5, 5.5, 1, 1, 1, 1, 700, 500, 100, 200, 100, 50, 100, 30000, 20, 1, 1, 1, 1, 1, 1]
+
+@app.route("/upload", methods=["POST", "GET"])
+def file():
+	if request.method == "POST":
+		if request.files:
+			f = request.files['data_file']
+
+			if f:
+				stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
+				data = list(csv.reader(stream))
+				print(data)
+
+				for i in range(24):
+					data[0][i] = float((float(data[0][i]) - min[i]) / (max[i] - min[i]))
+
+				res = model.predict(data)[0]
+				return redirect(url_for("result", res=res))
+		else:
+			return "no files added"
+	else:
+		return render_template("index.html")
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -13,15 +41,8 @@ def home():
 		for n in names:
 			data.append(float(request.form.get(n)))
 		
-		print(data)
-
-		min = [0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2000, 0, 0, 0, 0, 0, 0, 0]
-		max = [120, 360, 1.1, 5.5, 5.5, 1, 1, 1, 1, 700, 500, 100, 200, 100, 50, 100, 30000, 20, 1, 1, 1, 1, 1, 1]
-		
 		for i in range(24):
 			data[i] = (data[i] - min[i]) / (max[i] - min[i])
-
-		model = pickle.load(open('evc.pickle', 'rb'))
 
 		res = model.predict([data])[0]
 		return redirect(url_for("result", res=res))
